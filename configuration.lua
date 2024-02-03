693ace5bc68309460f3a64059815ed592385f2fa
fosantos <fosantos@gmail.com>
2024-02-03 19:18:12 +0000

    Update configuration.lua

--- configuration.lua
+++ configuration.lua
@@ -1,7 +1,7 @@
 config = {}
 
-config.url = "mongodb+srv://staging_dev:dev-pwd@staging-pl-0.cb4tn.mongodb.net/poi-representation?retryWrites=true&w=majority"
-config.database = "poi-representation"
-config.collection = "poi-representation_poirepresentations"
+config.url = "mongodb+srv://XXXXXXXX"
+config.database = "XXXXXn"
+config.collection = "XXXX"
 
 return config
